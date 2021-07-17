101346cf2a8ab94071a1768b51cb230cbf56f3ec
ygorpinto <ygor.pinto@hotmail.com>
2021-07-17 20:41:23 -0300

    ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # aqui ficaria algo semelhante a uma documentação ou um controller do swagger mesmo
     render json: "Favorites API"
   end
 end
